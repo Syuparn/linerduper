1c74bef78a2700da792f2a91335dadccd2756cae
syuparn <s.hello.spagetti@gmail.com>
2023-11-22 23:12:28 +0900

    init project

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # linerduper
 A small text editor for one-liner lovers (working offline)
+
+
+# for developers
+
+- `npm run dev`: run locally
+- `npm build`: build to static app
---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// NOTE: for security reason, SharedArrayBuffer(, which is required for wasm runtime) cannot run without these headers!
+const crossOriginPolicy = {
+  name: "configure-server",
+
+  configureServer(server) {
+    server.middlewares.use((_req, res, next) => {
+      res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
+      res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
+      next();
+    });
+  },
+};
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), crossOriginPolicy],
+})
