7dcbcd37e39feccae767a6a94fa1ecee3b60ba55
syuparn <s.hello.spagetti@gmail.com>
2023-11-24 09:26:57 +0900

    add pangaea

--- src/wasm/handler.ts
+++ src/wasm/handler.ts
@@ -1,4 +1,5 @@
-import { RunResult, headlessRunCode } from "@runno/runtime"
+import { CompleteResult, RunResult, headlessRunCode } from "@runno/runtime"
+import { WASI } from "@runno/wasi"
 
 type WASIRunner = (sourceCode: string, text: string) => Promise<RunResult>
 
@@ -7,6 +8,50 @@ export const wasiRunners: { [key: string]: WASIRunner } = {
   python: (sourceCode, text) => headlessRunCode('python', sourceCode, text+'\n'),
   ruby: (sourceCode, text) => headlessRunCode('ruby', sourceCode, text+'\n'),
   quickjs: (sourceCode, text) => headlessRunCode('quickjs', sourceCode, text+'\n'),
+  pangaea: (sourceCode, text) => wrapWASIRunner('/wasm/pangaea.wasm', text+'\n', ['-e', sourceCode]),
 }
 
 export const runnerCommands = Object.keys(wasiRunners)
+
+async function wrapWASIRunner(path: string, stdin: string , args: string[]) {
+  const prepare: CompleteResult = {
+    resultType: "complete",
+    stdin: stdin,
+    stdout: "",
+    stderr: "",
+    tty: "",
+    exitCode: 0,
+    fs: {}, // set later
+  }
+
+  // NOTE: WASIWorkerHost cannot be used for external binaries
+  const result = await WASI.start(fetch(path), {
+    // HACK: separate by -- otherwise args are passed to wasi runner directly
+    args: ['--', ...args],
+    stdout: out => {
+      prepare.stdout += out
+      prepare.tty += out
+    },
+    stderr: err => {
+      prepare.stderr += err
+      prepare.tty += err
+    },
+    // NOTE: null is required to tell worker to stop reading
+    stdin: (() => {
+      // HACK: use closure to send stdin only once
+      let done = false
+      return () => {
+        if (done) {
+          return null
+        }
+        done = true
+        return stdin
+      }
+    })(),
+  })
+
+  prepare.fs = result.fs
+  prepare.exitCode = result.exitCode
+
+  return prepare
+}
